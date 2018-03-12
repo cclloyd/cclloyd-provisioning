026ea780c1e8ed8689d56aedf3254eedec326771
Michael <cclloyd9786@gmail.com>
2018-03-12 19:45:27 -0400

    updated

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,10 +2,24 @@ class profile::base {
 	class { '::ntp': }
 	
 	
-	class { 'ufprovisioning':
-		site_name => "puppetmaster.cclloyd.com",
-		webserver_manage => true,
+	node 'cclloyd.com' {
+		class { 'ufprovisioning':
+			site_name => "cclloyd.com",
+			webserver_manage => true,
 	}
+	}
+	
+	node 'puppetmaster.cclloyd.com {
+		class { 'ufprovisioning':
+			site_name => "puppetmaster.cclloyd.com",
+			webserver_manage => true,
+		}
+	}
+	
+	#class { 'ufprovisioning':
+	#	site_name => "puppetmaster.cclloyd.com",
+	#	webserver_manage => true,
+	#}
 	
 	
 	
