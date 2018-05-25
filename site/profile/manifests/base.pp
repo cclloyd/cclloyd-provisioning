7497a24b04d497f7b2c45509730348106db17085
Michael <cclloyd9786@gmail.com>
2018-05-25 02:33:57 -0400

    updated

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -10,36 +10,13 @@ class profile::base {
 		
 		
 		class 	{ 'ufprovisioning::michael': }
-		#class 	{ 'ufprovisioning::bittorrent': }
 		class 	{ 'ufprovisioning::plex': }
 		class 	{ 'ufprovisioning::git': }
-		#class	{ 'ufprovisioning::grafana': }
 		class 	{ 'ufprovisioning::cclloyd': }
-		#class	{ 'ufprovisioning::userfrosting': }
-		#class	{ 'ufprovisioning::cloud9': }
-		#class	{ 'ufprovisioning::django': }
-		#class	{ 'ufprovisioning::owncloud': }
 		class	{ 'ufprovisioning::reactstack': }
 
 	}
-	
-	node 'puppetmaster.cclloyd.com' {
-		class { 'ufprovisioning':
-			site_name => "puppetmaster.cclloyd.com",
-			webserver_manage => true,
-		}
-		
-		class { 'ufprovisioning::michael': }
-		#class { 'ufprovisioning::bittorrent': }
-		#class { 'ufprovisioning::plex': }
-		class { 'ufprovisioning::git': }
-		#class { 'ufprovisioning::grafana': }
-		class { 'ufprovisioning::cclloyd': }
-		#class { 'ufprovisioning::userfrosting': }
-		class { 'ufprovisioning::django': }
-		#class { '::router': }
-	}
-	
+
 	node 'opensrd.cclloyd.com' {
 		class { 'ufprovisioning':
 			site_name => "opensrd.cclloyd.com",
